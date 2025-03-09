d62c949c4689ec65c6a052afd704ca3076c7e1c1
Yevhen Anisimov <yevhen.anisimov@ukma.edu.ua>
2025-03-09 14:12:03 +0200

    Initial Commit

---
+++ SwiftAppTests/SwiftAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppTests.swift
+//  SwiftAppTests
+//
+//  Created by Евгений  Анисимов  on 3/9/25.
+//
+
+import Testing
+@testable import SwiftApp
+
+struct SwiftAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
