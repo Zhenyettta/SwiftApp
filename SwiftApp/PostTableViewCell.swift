7189a698dba5d8fd786b305668b199c13411cdca
Yevhen Anisimov <yevhen.anisimov@ukma.edu.ua>
2025-03-16 23:02:07 +0200

    done, idk how to fix different image sizes and view stretching

---
+++ SwiftApp/PostTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostTableViewCell.swift
+//  SwiftApp
+//
+//  Created by Евгений  Анисимов  on 3/16/25.
+//
+
+import SwiftUI
+
+struct PostTableViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostTableViewCell()
+}
